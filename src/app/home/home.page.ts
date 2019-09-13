f499cc07fceea1a60fef5a54a076b7190d2863a5
Eric Bischoff <ericbischoff@a4-83-e7-6f-50-5b.wlan.lsu.edu>
2019-09-13 11:07:01 -0500

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public cssClass: string;
+  private answers = ['Abita Purple Haze', 'Coors Lite', 'Bud Light',
+             'You should stay sober today', 'Michelob Ultra', 'Miller Genuine Draft',
+             'Tecate', 'Bud Light Lime', 'Guinness',
+             'Corona Light', 'Coors Banquet', 'Dos Equis',
+             'Blue Moon', 'Pabst Blue Ribbon',
+             'Yuengling Lager', "Stella Artois", 'Miller High Life',
+             'Heineken', 'Busch', 'Natural Light', 'Modelo Especial', 'Corona Extra', 'Budweiser', 'Miller Lite']
 
-  constructor() {}
+  constructor(private toastCtrl: ToastController) {}
+
+  giveAnswer() {
+    this.cssClass = "animated shake";
+    let answer = this.answers[Math.floor(Math.random() * this.answers.length)];
+    // this.presentToast(answer);
+
+    var temp = this;
+    setTimeout(function(){
+      temp.presentToast(answer);
+    }, 2000)
+  }
+
+  async presentToast(answer) {
+    const toast = await this.toastCtrl.create({
+      message: answer,
+      duration: 5000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss().then(() => {
+      console.log('toast dismissed');
+      this.cssClass = "";
+    });
+    toast.present();
+  }
 
 }
